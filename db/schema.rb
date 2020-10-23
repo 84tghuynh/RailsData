6537ad5b2b404165963b3e7b60487f2ea7321585
84tghuynh <giangbrvt@gmail.com>
2020-10-22 22:25:52 -0500

    First,Migrate DB for Book, Author, BookAuthor

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_10_23_025526) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "authorKey"
+    t.string "name"
+    t.string "personalName"
+    t.text "bio"
+    t.text "cover_s"
+    t.text "cover_m"
+    t.text "cover_l"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["book_id"], name: "index_book_authors_on_book_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "ISBN"
+    t.string "title"
+    t.text "description"
+    t.string "publisher"
+    t.string "publishDate"
+    t.integer "numberOfPages"
+    t.text "bookURL"
+    t.text "cover_s"
+    t.text "cover_m"
+    t.text "cover_l"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
+end
