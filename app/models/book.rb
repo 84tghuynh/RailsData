918d246be400ffb1caee3006ff85ed0ee42ea65e
84tghuynh <giangbrvt@gmail.com>
2020-10-24 10:01:04 -0500

    Adding association between book and category. And add validate to category

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  belongs_to :category
   has_many :book_authors
   has_many :authors, through: :book_authors
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :books
+
+  validates :name, presence: true, uniqueness: true
 end
