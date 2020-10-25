b58b26b9ebbaa938c73569f3ce60e9ce462373bf
84tghuynh <giangbrvt@gmail.com>
2020-10-25 17:03:12 -0500

    Adding _header _partial, adding code to index action to Home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @books = Book.includes(:category)
+                 .limit(10)
+
+    @authors = Author.all
+                     .litmit(10)
   end
 
-  def about_data
-  end
+  def about_data; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  #get "home/index"
-
-
-  root to: 'home#index'
-  get "home/about_data"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "home/about_data"
+  root to: "home#index"
 end
