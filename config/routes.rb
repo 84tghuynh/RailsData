0b004da863f35229cafc5cdb6646d0bc14c40301
84tghuynh <giangbrvt@gmail.com>
2020-10-25 19:45:33 -0500

    Change default routes of authors to a new route, especially, change ' # get authors/show' to 'get authors/:id, to: authors#show,  as: author'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'authors/index'
-  get 'authors/show'
+  # get "authors/index"
+  # get "authors/show"
+  get "authors",     to: "authors#index", as: "authors" # products_path
+  get "authors/:id", to: "authors#show",  as: "author"
+  # resources :authors, only: %i[index show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "home/about_data"
   root to: "home#index"
