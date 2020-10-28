afae0892f1b76afe6b22e10ee7c5ed1875dd20f1
84tghuynh <giangbrvt@gmail.com>
2020-10-27 23:22:41 -0500

    Generate the category controller and create search functions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,18 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  def show
+  def show; end
+
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    category_search = params[:category].to_s
+
+    @books = if category_search == "0"
+               Book.where("title LIKE ?", wildcard_search)
+             else
+               Book.joins(:category)
+                   .where("title LIKE ?", wildcard_search)
+                   .where("category_id = ?", category_search)
+             end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
+  # get 'categories/index'
+  # get 'categories/show'
+  resources :categories, only: %i[index show]
+
   # get 'customers/index'
   # get 'customers/show'
   # resources :customers, only: %i[index show]
-
+  # OR
   get "customers",     to: "customers#index", as: "customers" # books_path
   get "customers/:id", to: "customers#show",  as: "customer"
+
   # get 'books/index'
   # get 'books/show'
   # using
-  # resources :books, only: %i[index show]
+  resources :books, only: %i[index show] do
+    collection do
+      get "search"
+    end
+  end
   # OR
-  get "books",     to: "books#index", as: "books" # books_path
-  get "books/:id", to: "books#show",  as: "book"
+  # get "books",     to: "books#index", as: "books" # books_path
+  # get "books/:id", to: "books#show",  as: "book"
 
   # get "authors/index"
   # get "authors/show"
